5c63a8120c32eb37d17c61bd244065188ffb149a
Vedansh270 <76037106+Vedansh270@users.noreply.github.com>
2022-09-21 17:59:20 +0530

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>Spectagram</Text>
+            </View>
+          </View>
+          <View style={styles.cardContainer}>
+            <FlatList
+              keyExtractor={this.keyExtractor}
+              data={posts}
+              renderItem={this.renderItem}
+            />
+          </View>
+        </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+  },
+  cardContainer: {
+    flex: 0.85
+  }
+});
